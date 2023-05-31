8ca856986440aca69f03eded24d181fb5580e902
riadev23 <riadev@proton.me>
2023-05-31 10:40:08 +0900

    정렬, 글제목 변경

--- src/App.js
+++ src/App.js
@@ -7,18 +7,32 @@ import { useState } from 'react';
 function App() {
 
   let post = '역삼 우동 맛집';
-  let [글제목, b] = useState(['남자코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
+
+      <span onClick={ () => {
+        let copy = [...글제목].sort();
+        글제목변경(copy);
+      }}>가나다순 정렬</span><br />
+
+      <span onClick={ () => {
+        let copy = [...글제목];
+        copy[0] = '여자코트 추천';
+        글제목변경(copy);
+      }}>글수정</span>
       
       <div className="list">
-        <h4>{ 글제목[0] } <span>👍</span> 0 </h4>
+        <h4>{ 글제목[0] } <span onClick={ () => { 따봉변경(따봉+1) } }>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
       </div>
+
+
 
       <div className="list">
         <h4>{ 글제목[1] }</h4>
